c84c78ce1f16d3ab6bc0c4ad09b6a2bcde84ef47
RafaelPreziaGomes <rafafprezia@gmail.com>
2022-07-10 22:43:35 -0400

    fixed port assigment

--- server.js
+++ server.js
@@ -2,6 +2,6 @@
 const app = require("./app");
 
 // // listen for requests on port 3000 and log to console when requests are received
-app.listen(3000, function () {
+app.listen(process.env.PORT, function () {
   console.log("Server listening on port 3000!");
 });
