ad3c916f37d5fe82b93e8ab2bbdc5d06cc24b4e2
rafafprezia <60278396+RafaelPreziaGomes@users.noreply.github.com>
2022-07-10 22:49:21 -0400

    Update server.js

--- server.js
+++ server.js
@@ -2,6 +2,6 @@
 const app = require("./app");
 
 // // listen for requests on port 3000 and log to console when requests are received
-app.listen(PORT, function () {
+app.listen(process.env.PORT || 3000, function () {
   console.log("Server listening on port 3000!");
 });
